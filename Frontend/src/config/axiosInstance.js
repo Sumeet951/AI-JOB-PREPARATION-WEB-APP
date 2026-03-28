c5e9e0f08207a528fbc2790ce2c64d86ded8e1ec
Sumeet951 <sg297979@gmail.com>
2026-03-28 19:11:20 +0530

    Add Backend

--- Frontend/src/config/axiosInstance.js
+++ Frontend/src/config/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "https://ai-job-preparation-web-app.onrender.com/api";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
