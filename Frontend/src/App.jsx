36b7fec0f0562270dd51ebf295ccf8aac49c8332
Sumeet951 <sg297979@gmail.com>
2026-03-28 18:19:30 +0530

    Add login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {Route,Routes} from "react-router-dom"
-import Login from './features/auth/pages/login'
+import Login from './features/auth/pages/Login'
 import Register from './features/auth/pages/Register'
 import Home from './features/interview/pages/Home'
 import Interview from './features/interview/pages/interview'
